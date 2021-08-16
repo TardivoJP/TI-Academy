ac3ad432f921c49e3dabeb6df61b18a600c72b88
TardivoJP <tardivojoaopedro@gmail.com>
2021-08-16 20:55:00 -0300

    Atualização script.js
    Mudança para utilizar um loop while para confirmar se o usuário digitou um número ou não. Anteriormente, o código utilizava uma forma if & else em que o usuário teria que atualizar a página para inserir outro valor, o que era uma solução deselegante.

--- Ciclo 2 - Javascript/Dia 1/Desafio - 16.08/script.js
+++ Ciclo 2 - Javascript/Dia 1/Desafio - 16.08/script.js
@@ -3,9 +3,14 @@ var n = 15;
 
 var nome = prompt("Digite seu nome");
 
-var numeroString = prompt(nome+" , digite um numero para comparar com 'n'");
+var numeroString = prompt(nome+", digite um numero para comparar com 'n'");
 
 var numeroVerdadeiro = parseInt(numeroString);
+
+while(numeroVerdadeiro !== numeroVerdadeiro){
+	var numeroString = prompt("Vamos com calma "+nome+"!\n A caixinha te pediu um número!\nTente digitar novamente!");
+	var numeroVerdadeiro = parseInt(numeroString);
+}
 
 var compara = (n == numeroVerdadeiro);
 
@@ -19,20 +24,17 @@ var quadrado = Math.pow(numeroVerdadeiro, 2);
 
 document.write("1. Olá, <b>"+nome+"</b>!"+"<br>");
 
-if(numeroVerdadeiro !== numeroVerdadeiro){
-	document.write("2. Vamos com calma! A caixinha te pediu um número!<br>3. Aperte F5 e tente novamente!")
+document.write("2. Seu número é: <b>"+numeroVerdadeiro+"</b>."+"<br>");
+
+if(compara === true){
+	document.write("3. Seu número é igual a <b>n</b>!"+"<br>");
 }else{
-	document.write("2. Seu número é: <b>"+numeroVerdadeiro+"</b>."+"<br>");
-
-	if(compara === true){
-		document.write("3. Seu número é igual a <b>n</b>!"+"<br>");
-	}else{
-		document.write("3. Seu número é diferente de <b>n</b>!"+"<br>");
-	}
-
-	document.write("4. O retorno da comparação booleana entre ele e <b>n</b> é: <b>"+compara+"</b>."+"<br>");
-	document.write("5. A soma dele e <b>n</b> é: <b>"+soma+"</b>."+"<br>");
-	document.write("6. A subtração dele e <b>n</b> é: <b>"+subtracao+"</b>."+"<br>");
-	document.write("7. O resto da divisão entre ele e <b>n</b> é: <b>"+modulo+"</b>."+"<br>");
-	document.write("8. O seu número elevado ao quadrado é: <b>"+quadrado+"</b>.");
+	document.write("3. Seu número é diferente de <b>n</b>!"+"<br>");
 }
+
+document.write("4. O retorno da comparação booleana entre ele e <b>n</b> é: <b>"+compara+"</b>."+"<br>");
+document.write("5. A soma dele e <b>n</b> é: <b>"+soma+"</b>."+"<br>");
+document.write("6. A subtração dele e <b>n</b> é: <b>"+subtracao+"</b>."+"<br>");
+document.write("7. O resto da divisão entre ele e <b>n</b> é: <b>"+modulo+"</b>."+"<br>");
+document.write("8. O seu número elevado ao quadrado é: <b>"+quadrado+"</b>.");
+
