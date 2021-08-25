a9af66371c48e91cb7bada275752ed9270e2a634
TardivoJP <tardivojoaopedro@gmail.com>
2021-08-25 13:08:10 -0300

    Melhorias no Desafio - 24.08
    Alguns CEPs não contém todas as informações necessárias para preencher os campos, logo foram adicionadas algumas exceções:
    	*Rua não encontrada;
    	*Bairro não encontrado;
    	*Cidade não encontrada;
    	*UF não encontrada;
    
    Além disso, agora o programa retorna uma caixa de alerta com um erro caso o usuário digite um CEP inválido.

--- Ciclo 2 - Javascript/Dia 5/Desafio - 24.08/ceps.js
+++ Ciclo 2 - Javascript/Dia 5/Desafio - 24.08/ceps.js
@@ -5,16 +5,35 @@ window.onload=function(){
 
 	const dadosCep=async function(cepUrl){
 
+	try{
 		var url=`https://viacep.com.br/ws/${cepUrl}/json/`;
 		var consultaCep=await fetch(url);
 		var dadoCep=await consultaCep.json()
 
 		for(var campino in dadoCep){
-			console.log(campino);
+
 			if(document.querySelector("#"+campino)){
 			document.querySelector("#"+campino).value=dadoCep[campino];
+			console.log(document.querySelector("#logradouro").value);
+				if((document.querySelector("#logradouro").value)==""){
+					document.querySelector("#logradouro").value="Rua não encontrada"
+				}
+				if((document.querySelector("#bairro").value)==""){
+					document.querySelector("#bairro").value="Bairro não encontrado"
+				}
+				if((document.querySelector("#localidade").value)==""){
+					document.querySelector("#localidade").value="Cidade não encontrada"
+				}
+				if((document.querySelector("#uf").value)==""){
+					document.querySelector("#uf").value="UF não encontrada"
+				}
 			}
 		}
+
+		
+	}catch(erro){
+		alert(`Erro! O valor digitado na caixa de cep é inválido!`)
+	 }
 	}
 
 	btnBuscaCep.addEventListener("click",()=>{
